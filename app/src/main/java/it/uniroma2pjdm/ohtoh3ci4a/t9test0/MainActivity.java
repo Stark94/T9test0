0098118d92b0402f1436d69ff89d3b700f3c697a
Stark94 <ciotta.pa@hotmail.it>
2018-07-24 14:30:09 +0200

    Initial commit

---
+++ app/src/main/java/it/uniroma2pjdm/ohtoh3ci4a/t9test0/MainActivity.java
@@ -0,0 +1,13 @@
+package it.uniroma2pjdm.ohtoh3ci4a.t9test0;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
